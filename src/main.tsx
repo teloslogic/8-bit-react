483d2e73506aa0d2c4866df7fe6539ac2dcf7ad6
Craig Lubin <lubin.craig@gmail.com>
2025-04-17 23:00:28 -0400

    Add tsconfig

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import './index.css'
 
 import { Provider } from 'jotai'
-import React from 'react'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
